05abfe0f90ccd2026efa697ffb6f837c1935f7c3
Pavel Kundenko <pavel.kundenko@gmail.com>
2023-02-04 16:50:31 +0200

    feat: adds help command

--- src/modules/start/start.service.ts
+++ src/modules/start/start.service.ts
@@ -1,10 +1,26 @@
 import { injectable } from 'inversify';
 import { TCommandContext } from '@typings/telegraf';
+import { ESteamCommand } from '@modules/steam/steam.commands';
 import { IStartService } from './start.interface';
 
 @injectable()
 export class StartService implements IStartService {
-  start(ctx: TCommandContext) {
+  start = (ctx: TCommandContext) => {
     ctx.reply('Hello, I\'m Team Fiasko bot');
-  }
+    ctx.reply(this.getHelpText());
+  };
+
+  help = (ctx: TCommandContext) => {
+    ctx.reply(this.getHelpText());
+  };
+
+  private getHelpText = () => {
+    const commands = [
+      `${ESteamCommand.Sales} - get current steam top 10 sales`,
+      `${ESteamCommand.SubscribeSales} - subscribe to daily steam sales update`,
+      `${ESteamCommand.UnsubscribeSales} - unsubscribe from steam sales`,
+    ];
+
+    return `Available commands:\n${commands.join('\n')}`;
+  };
 }
